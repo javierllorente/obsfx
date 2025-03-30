e3973361ebfa96335523031306d94bc1234e6c5a
Javier Llorente <javier@opensuse.org>
2025-03-30 20:34:40 +0200

    Update modules

--- obsfx.sh
+++ obsfx.sh
@@ -10,6 +10,7 @@ lib_dir=$base_dir/dist/lib
 
 module_path=$JAVAFX_HOME:$lib_dir
 modules=javafx.controls,javafx.fxml,org.kordamp.ikonli.javafx,org.controlsfx.controls
+modules+=,jakarta.inject,jakarta.annotation,org.apache.logging.log4j.slf4j2.impl
 modules+=,com.javierllorente.jobs
 exports=javafx.base/com.sun.javafx.event=org.controlsfx.controls
 
