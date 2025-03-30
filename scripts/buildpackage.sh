fd989ce640261f73621035f422d5f2cf615f6cd8
Javier Llorente <javier@opensuse.org>
2025-03-30 20:33:48 +0200

    Use assembleruntime.sh

--- scripts/buildpackage.sh
+++ scripts/buildpackage.sh
@@ -19,13 +19,16 @@ release=0
 
 JAVA_HOME=/usr/lib64/jvm/java-17-openjdk-17
 JAVAFX_HOME=$build_dir/javafx
-jpackage_path=$JAVA_HOME/bin/
+jpackage_path=$JAVA_HOME/bin
 module_path=:$JAVAFX_HOME:$lib_dir
-modules=javafx.controls,javafx.fxml,java.base,java.logging,java.prefs,jdk.crypto.ec,java.naming
+runtime_dir=$build_dir/runtime
 java_options='--add-exports javafx.base/com.sun.javafx.event=ALL-UNNAMED'
+
+echo "Assembling runtime..."
+./assembleruntime.sh
 
 echo "Building package..."
 $jpackage_path/jpackage --verbose --type rpm --app-version $version --linux-app-release $release \
 --linux-rpm-license-type $license_type --license-file $license_file \
---input $dist_dir --main-jar $jar_filename --name $name --module-path $module_path --add-modules $modules \
+--input $dist_dir --main-jar $jar_filename --name $name --module-path $module_path --runtime-image $runtime_dir \
 --java-options "$java_options" --icon $icon --linux-shortcut --linux-menu-group $menu_group --dest $build_dir
